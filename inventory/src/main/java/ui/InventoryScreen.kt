c9c6067800f16bfd9853cf6d409046fed466086a
Joaquin Alan Alvidrez Soto <joaquinalvidrez95@gmail.com>
2022-03-07 22:02:06 -0500

    Add products to table

--- inventory/src/main/java/ui/InventoryScreen.kt
+++ inventory/src/main/java/ui/InventoryScreen.kt
@@ -1,34 +1,41 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import domain.Product
 
 @Composable
 fun InventoryScreen() {
-    Column {
+    val products = arrayOf(
+        Product(name = "Wheel", price = 34.2F, manufacturer = "Bosch", category = ""),
+        Product(name = "Tire", price = 4.2F, manufacturer = "Bosch", category = ""),
+        Product(name = "Oil", price = 34.2F, manufacturer = "Continental", category = ""),
+        Product(name = "Wheel", price = 34.2F, manufacturer = "Bosch", category = ""),
+        Product(name = "Wheel", price = 34.2F, manufacturer = "Bosch", category = ""),
+    )
 
-        repeat(times = 20) {
-            Item()
+    LazyColumn {
+        items(items = products) {
+            Item(it)
         }
     }
-
 }
 
 @Composable
-private fun Item() {
+private fun Item(product: Product) {
     Row {
-        Text("Hello")
-        Text("Bosch")
-        Text("34")
+        Text(product.name)
+        Text("\$${product.price}")
+        Text(product.manufacturer)
     }
 }
 
 @Preview
 @Composable
 private fun ItemPreview() {
-    Item()
+    //Item(it)
 }
 
 @Preview
